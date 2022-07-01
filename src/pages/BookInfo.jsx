36343aca4aa1fac7a69c96d888a7e9134f809a05
aryanprince <aryanprincesanty@icloud.com>
2022-07-01 23:36:18 +0530

    1:35:48 Added individual book info pages

--- src/pages/BookInfo.jsx
+++ src/pages/BookInfo.jsx
@@ -1,10 +1,14 @@
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { Link, useParams } from 'react-router-dom'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
+import Price from '../components/ui/Price'
 import Rating from '../components/ui/Rating'
 
 export default function BookInfo({ books }) {
+    const { id } = useParams()
+    const book = books.find((book) => +book.id === +id)
+
     return (
         <div id="books__body">
             <main id="books__main">
@@ -21,13 +25,36 @@ export default function BookInfo({ books }) {
 
                         <div className="book__selected">
                             <figure className="book__selected--figure">
-                                <img src="https://covers.openlibrary.org/b/id/8091016-L.jpg" alt="" className="book__selected--img" />
+                                <img src={book.url} alt="" className="book__selected--img" />
                             </figure>
 
                             <div className="book__selected--description">
-                                <h2 className="book__selected--title">Crack the Coding Interview</h2>
-                                <Rating rating="4.5" />
+                                <h2 className="book__selected--title">{book.title}</h2>
+                                <Rating rating={book.rating} />
+                                <div className="book__selected--price">
+                                    <Price salePrice={book.salePrice} originalPrice={book.originalPrice} />
+                                </div>
+                                <div className="book__summary">
+                                    <h3 className="book__summary--title">Summary</h3>
+                                    <p className="book__summary--para">
+                                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque vel voluptates mollitia adipisci hic commodi id alias obcaecati, provident quasi sequi nam!
+                                        Numquam, obcaecati in nobis assumenda sed officia ducimus.
+                                    </p>
+                                    <p className="book__summary--para">
+                                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque vel voluptates mollitia adipisci hic commodi id alias obcaecati, provident quasi sequi nam!
+                                        Numquam, obcaecati in nobis assumenda sed officia ducimus.
+                                    </p>
+                                    <button className="btn">Add to Cart</button>
+                                </div>
                             </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="book__container">
+                    <div className="row">
+                        <div className="book__selected--top">
+                            <h2 className="book__selected--title--top">Recommended Books</h2>
                         </div>
                     </div>
                 </div>
