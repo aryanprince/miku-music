23070edf346b1f4d69e94318514ccd99a687c6f3
aryanprince <aryanprincesanty@icloud.com>
2022-07-02 17:01:33 +0530

    Error with skeletal loading

--- src/components/ui/Book.jsx
+++ src/components/ui/Book.jsx
@@ -1,31 +1,60 @@
-import React from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import { Link } from 'react-router-dom'
 
 import Price from './Price'
 import Rating from './Rating'
 
 export default function Book({ book }) {
+    const [img, setImg] = useState()
+
+    const mountedRef = useRef(true)
+
+    useEffect(() => {
+        const image = new Image()
+        image.src = book.url
+        image.onload = () => {
+            if (mountedRef.current) {
+                setImg(image)
+            }
+        }
+        return () => {
+            //When the component unmounts
+            mountedRef.current = false
+        }
+    })
+
     return (
         <div className="book">
-            {/* BOOK IMAGE */}
-            <Link to={`/books/${book.id}`}>
-                <figure className="book_img-wrapper">
-                    <img src={book.url} alt="" />
-                </figure>
-            </Link>
+            {img ? (
+                <>
+                    {/* BOOK IMAGE */}
+                    <Link to={`/books/${book.id}`}>
+                        <figure className="book_img-wrapper">
+                            <img src={img.src} alt="" />
+                        </figure>
+                    </Link>
 
-            {/* BOOK TITLE */}
-            <div className="book__title">
-                <Link to={`/books/${book.id}`} className="book__title--link">
-                    {book.title}
-                </Link>
-            </div>
+                    {/* BOOK TITLE */}
+                    <div className="book__title">
+                        <Link to={`/books/${book.id}`} className="book__title--link">
+                            {book.title}
+                        </Link>
+                    </div>
 
-            {/* BOOK RATING */}
-            <Rating rating={book.rating} />
+                    {/* BOOK RATING */}
+                    <Rating rating={book.rating} />
 
-            {/* BOOK PRICE */}
-            <Price salePrice={book.salePrice} originalPrice={book.originalPrice} />
+                    {/* BOOK PRICE */}
+                    <Price salePrice={book.salePrice} originalPrice={book.originalPrice} />
+                </>
+            ) : (
+                <>
+                    <div className="book__img--skeleton"></div>
+                    <div className="skeleton book__title--skeleton"></div>
+                    <div className="skeleton book__rating--skeleton"></div>
+                    <div className="skeleton book__price--skeleton"></div>
+                </>
+            )}
         </div>
     )
 }
