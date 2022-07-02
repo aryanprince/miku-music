7ff6c7be769ca244f2bf6b4d52082c76ec5ae8ed
aryanprince <aryanprincesanty@icloud.com>
2022-07-02 14:13:48 +0530

    Added comments

--- src/pages/BookInfo.jsx
+++ src/pages/BookInfo.jsx
@@ -23,6 +23,7 @@ export default function BookInfo({ books, addToCart, cart }) {
             <main id="books__main">
                 <div className="books__container">
                     <div className="row">
+                        {/* BACK BUTTON TO GO TO LIST OF BOOKS */}
                         <div className="book__selected--top">
                             <Link to="/books" className="book__link">
                                 <FontAwesomeIcon icon="arrow-left" />
@@ -32,17 +33,25 @@ export default function BookInfo({ books, addToCart, cart }) {
                             </Link>
                         </div>
 
+                        {/* CURRENTLY SELECTED BOOK */}
                         <div className="book__selected">
+                            {/* BOOK IMAGE */}
                             <figure className="book__selected--figure">
                                 <img src={book.url} alt="" className="book__selected--img" />
                             </figure>
 
+                            {/* BOOK DESCRIPTION */}
                             <div className="book__selected--description">
+                                {/* BOOK TITLE */}
                                 <h2 className="book__selected--title">{book.title}</h2>
+                                {/* BOOK RATING */}
                                 <Rating rating={book.rating} />
+                                {/* BOOK PRICE */}
                                 <div className="book__selected--price">
                                     <Price salePrice={book.salePrice} originalPrice={book.originalPrice} />
                                 </div>
+
+                                {/* BOOK SUMMARY */}
                                 <div className="book__summary">
                                     <h3 className="book__summary--title">Summary</h3>
                                     <p className="book__summary--para">
@@ -54,6 +63,7 @@ export default function BookInfo({ books, addToCart, cart }) {
                                         Numquam, obcaecati in nobis assumenda sed officia ducimus.
                                     </p>
 
+                                    {/* ADD TO CART / CHECKOUT BUTTON */}
                                     {bookExistsOnCart() ? (
                                         <Link to={`/cart`} className="book__link">
                                             <button className="btn">Checkout</button>
@@ -69,6 +79,7 @@ export default function BookInfo({ books, addToCart, cart }) {
                     </div>
                 </div>
 
+                {/* ADDITIONAL RECOMMENDED BOOKS */}
                 <div className="book__container">
                     <div className="row">
                         <div className="book__selected--top">
