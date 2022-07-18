258bf3426b30cafb2819cb14ee9ff8389e39e708
aryanprince <aryanprincesanty@icloud.com>
2022-07-19 01:12:46 +0530

    Dummy album details added to data file

--- src/data.js
+++ src/data.js
@@ -7,6 +7,9 @@ export const albums = [
         url: 'https://is5-ssl.mzstatic.com/image/thumb/Music114/v4/44/e3/94/44e3945f-d613-09f8-538a-aba063d59358/20UM1IM14847.rgb.jpg/600x600bb.jpg',
         summary:
             'Working again with Aaron Dessner, Swift challenges herself to find new dimensions within the moody atmosphere: fingerpicked ballads, colorful pop music, and her first country songs in years.',
+        songs: 14,
+        duration: '56:17',
+        year: '2020',
         originalPrice: 49.95,
         salePrice: 14.95,
         rating: 4.5,
@@ -17,6 +20,9 @@ export const albums = [
         artist: 'Jaden',
         url: 'https://is3-ssl.mzstatic.com/image/thumb/Music114/v4/96/19/da/9619da38-78f2-04b7-02b1-5e741500ceac/20UMGIM68982.rgb.jpg/600x600bb.jpg',
         summary: 'No summary yet',
+        songs: 14,
+        duration: '56:17',
+        year: '2020',
         originalPrice: 39,
         salePrice: null,
         rating: 3,
@@ -28,6 +34,10 @@ export const albums = [
         url: 'https://is4-ssl.mzstatic.com/image/thumb/Music125/v4/ca/f3/67/caf367a5-2cf6-6b2e-a891-97dc57b19f08/20UMGIM64216.rgb.jpg/600x600bb.jpg',
         summary:
             "Made from afar, primarily with the National's Aaron Dessner, Swift's eighth album is a sweater-weather record filled with cinematic love songs and rich fictional details.",
+
+        songs: 14,
+        duration: '56:17',
+        year: '2020',
         originalPrice: 29,
         salePrice: null,
         rating: 5,
@@ -39,6 +49,10 @@ export const albums = [
         url: 'https://is1-ssl.mzstatic.com/image/thumb/Music126/v4/df/69/c8/df69c87c-3127-2327-68d1-9d0935301079/14UMGIM20562.rgb.jpg/600x600bb.jpg',
         summary:
             'The follow-up to Born to Die finds Lana Del Rey in ballad mode, finding new synergy between the character she presents to the world and the content of the songs. Producer Dan Auerbach of Black Keys turns out to be the perfect creative partner.',
+
+        songs: 14,
+        duration: '56:17',
+        year: '2020',
         originalPrice: 44,
         salePrice: 19,
         rating: 5,
@@ -50,6 +64,10 @@ export const albums = [
         url: 'https://is1-ssl.mzstatic.com/image/thumb/Music124/v4/67/6c/56/676c56ba-63f5-78e9-db04-44705f9c7211/5054429135121.png/600x600bb.jpg',
         summary:
             'The electronic duo Odesza are at the center of the current movement in stadium-filling “chill” music. Their latest is full of billowing vocal harmonies, seismic rumble, and turbo-charged trap beats.',
+
+        songs: 14,
+        duration: '56:17',
+        year: '2020',
         originalPrice: 32,
         salePrice: null,
         rating: 5,
@@ -60,6 +78,9 @@ export const albums = [
         artist: 'Harry Styles',
         url: 'https://is3-ssl.mzstatic.com/image/thumb/Music126/v4/2a/19/fb/2a19fb85-2f70-9e44-f2a9-82abe679b88e/886449990061.jpg/600x600bb.jpg',
         summary: "Harry Styles' melodious third solo album sometimes lacks substance, but style always abounds.",
+        songs: 14,
+        duration: '56:17',
+        year: '2020',
         originalPrice: 70,
         salePrice: 12.5,
         rating: 3.5,
@@ -71,6 +92,10 @@ export const albums = [
         url: 'https://is4-ssl.mzstatic.com/image/thumb/Music124/v4/05/c5/f5/05c5f579-085a-7ac7-2c93-b23ec638c451/20UMGIM14807.rgb.jpg/600x600bb.jpg',
         summary:
             "Selena Gomez's third album is a smooth and confident pop record that delves deep—but not that deep—into heartbreak, resilience, and self-love. ",
+
+        songs: 14,
+        duration: '56:17',
+        year: '2020',
         originalPrice: 11,
         salePrice: 10,
         rating: 4,
@@ -82,6 +107,9 @@ export const albums = [
         url: 'https://is3-ssl.mzstatic.com/image/thumb/Music124/v4/e5/49/ae/e549ae53-352c-779c-8a24-e3fc7b50e648/19UMGIM61350.rgb.jpg/600x600bb.jpg',
         summary:
             "On her elegant and complex fifth album, Lana Del Rey sings exquisitely of freedom and transformation and the wreckage of being alive. It establishes her as one of America's greatest living songwriters.",
+        songs: 14,
+        duration: '56:17',
+        year: '2020',
         originalPrice: 38,
         salePrice: 17.95,
         rating: 5,
@@ -93,6 +121,9 @@ export const albums = [
         url: 'https://is3-ssl.mzstatic.com/image/thumb/Music114/v4/5e/30/32/5e30329b-2765-60a6-96b8-8151c4b15a01/12UMGIM54175.rgb.jpg/600x600bb.jpg',
         summary:
             'After last year\'s hit "Video Games" and the endless remixes, leaks, think-pieces, and controversy that followed, it\'s finally here: the major-label debut from Lana Del Rey.',
+        songs: 14,
+        duration: '56:17',
+        year: '2020',
         originalPrice: 35,
         salePrice: 19.95,
         rating: 4.5,
@@ -103,6 +134,9 @@ export const albums = [
         artist: 'KSI',
         url: 'https://is2-ssl.mzstatic.com/image/thumb/Music115/v4/a5/ae/9c/a5ae9c84-f25d-69ef-abb0-92fb350b2d15/4050538708912.jpg/600x600bb.jpg',
         summary: 'No summary yet',
+        songs: 14,
+        duration: '56:17',
+        year: '2020',
         originalPrice: 40,
         salePrice: null,
         rating: 4,
@@ -113,6 +147,9 @@ export const albums = [
         artist: 'ODESZA',
         url: 'https://is4-ssl.mzstatic.com/image/thumb/Music122/v4/65/88/8f/65888f9c-e9ee-1e98-ef4c-66f98329e9a3/5054429154962.png/600x600bb.jpg',
         summary: 'To be released',
+        songs: 14,
+        duration: '56:17',
+        year: '2020',
         originalPrice: 30,
         salePrice: 20,
         rating: 4,
